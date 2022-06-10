a3d93d447e365ab30c066933080c914f6e308098
fernandocfbf <fernandocfbf@al.insper.edu.br>
2022-06-09 23:20:37 -0300

    feat: adding functions to collect and differ cookies

---
+++ popup/risk-detector.js
@@ -0,0 +1,32 @@
+function getActiveTab(){
+    /*returns the current tab infromation*/
+    return browser.tabs.query({currentWindow: true, active: true});
+}
+
+
+function isFirstParty(url, cookie){
+    /* returns true if an cookie is first party and false if it's not*/
+    return url.includes(cookie.domain);
+}
+
+function isPersistent(cookie){
+    /* returns true if an cookie is persistent and false if it's not*/
+    return cookie.expirationDate != undefined;
+}
+
+async function getCookies(tabs){
+    /* returns an object with information about the tab's cookies*/
+    const tab = tabs.pop();
+    const execute = browser.cookies.getAll({url: tab.url});
+    var tabInformation = {"firstParty": 0, "thirdParty": 0, "sessionParty": 0, "persistentParty": 0}
+    await execute.then((cookies) => {
+        console.log(cookies);
+        for (const cookie of cookies){
+            isFirstParty(tab.url, cookie) ? tabInformation['firstParty']+=1 : tabInformation['thirdParty']+=1;
+            isPersistent(cookie) ? tabInformation['persistentParty']+=1 : tabInformation['sessionParty']+=1;
+        }        
+    })   
+    return tabInformation;
+}
+
+getActiveTab().then(getCookies);
